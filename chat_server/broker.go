4468c3b2ea61af32fe11271c10882b4849566251
Carmen To <carmen.j.to@gmail.com>
2018-01-17 17:06:02 -0800

    completed chat server

--- chat_server/broker.go
+++ chat_server/broker.go
@@ -1,55 +1,79 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  // how to import external libraries
-  // you need to install `go dep`. dep is a dependency management for Golang (similar to npm and bundle). This will pull a source code from an open source repo!
-  // in the terminal:
-  // `dep init` - will check your source code to see if there is any missing libraries and it will create a file (go package) that will list all the things you need
-  // `dep ensure` - install all the missing dependencies from the package into a folder called vendor
-  "github.com/gorilla/websocket"
+	"fmt"
+	"net/http"
+	// how to import external libraries
+	// you need to install `go dep`. dep is a dependency management for Golang (similar to npm and bundle). This will pull a source code from an open source repo!
+	// in the terminal:
+	// `dep init` - will check your source code to see if there is any missing libraries and it will create a file (go package) that will list all the things you need
+	// `dep ensure` - install all the missing dependencies from the package into a folder called vendor
+	"github.com/gorilla/websocket"
 )
 
-
 type Payload struct {
-  // here we will add a json tag.
-  // when your server receive a json from the client, you want to unload the json into a struct. you just add a json tag to get the value
-  // e.g. received this json from client {email: "momo@to.com", username: "momoto", message: "meow"}
-  Email string `json:email`
-  Username string `json:username`
-  Message string `json:message`
+	// here we will add a json tag.
+	// when your server receive a json from the client, you want to unload the json into a struct. you just add a json tag to get the value
+	// e.g. received this json from client {email: "momo@to.com", username: "momoto", message: "meow"}
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
 }
 
 type Broker struct {
-  // map of websocket connections
-  ConnMap map[*websocket.Conn]bool
-  Broadcast chan Payload
-  // every websocket connect begins with a GET request will receive a request and server will immediately render a response. If we upgrade this request, we never return a response by keeping this connect alive. A long lived websocket connection!
-  Upgrader *websocket.Upgrader
+	// map of websocket connections
+	ConnMap   map[*websocket.Conn]bool
+	Broadcast chan Payload
+	// every websocket connect begins with a GET request will receive a request and server will immediately render a response. If we upgrade this request, we never return a response by keeping this connect alive. A long lived websocket connection!
+	Upgrader *websocket.Upgrader
+}
+
+// take payload from the Broadcast channel and Broadcast it to everyone who is connected to the server
+func (b *Broker) handleBroadcast() {
+	// listen to the Broadcast forever
+	for payload := range b.Broadcast {
+		// iterate through the connections
+		for conn := range b.ConnMap {
+			err := conn.WriteJSON(payload)
+			if err != nil {
+				fmt.Println("Failed to write JSON to websocket connection.")
+				conn.Close()
+				delete(b.ConnMap, conn)
+			}
+		}
+	}
 }
 
 // create a controller, a high level fn which returns a fn
-func (b *Broker) getHandleConnections() http.HandleFunc {
-  return func(w http.ResponseWriter, r *http.Request) {
-    conn, err := b.Upgrader.Upgrade(w, r, nil)
-    if err != nil {
-      fmt.Println("Failed to upgrade GET to a websocket connection")
-    }
-    // defer = excute this fn when the handler fn is done
-    defer conn.Close()
+func (b *Broker) getHandleConnections() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		conn, err := b.Upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Println("Failed to upgrade GET to a websocket connection")
+		}
+		// defer = excute this fn when the handler fn is done
+		defer conn.Close()
 
-    // we want to store the connection
-    b.ConnMap[conn] = true
-    for {
-      // constantly listen for Messages
-      var p Payload
-      if err := conn.ReadJSON(&p); err != nil {
-
-      } else {
-        fmt.Println("Incoming message:", p.Message)
-        
-      }
-    }
-  }
+		// we want to store the connection
+		b.ConnMap[conn] = true
+		for {
+			// constantly listen for Messages
+			var p Payload
+			if err := conn.ReadJSON(&p); err != nil {
+				if websocket.IsUnexpectedCloseError(err) {
+					fmt.Println("Client connection has closed")
+				} else {
+					fmt.Println("Encounter websocket error")
+				}
+				// delete live connention
+				delete(b.ConnMap, conn)
+				return
+			} else {
+				fmt.Println("Incoming message:", p.Message)
+				// send the message to the channel Broadcast
+				// one channel but multiple connection!
+				b.Broadcast <- p
+			}
+		}
+	}
 }
--- chat_server/server.go
+++ chat_server/server.go
@@ -1,18 +1,32 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
+  "github.com/gorilla/websocket"
 )
 
 func main() {
-  // FileServer will serve all the files in the public folder as a file server
-  fs := http.FileServer(http.Dir("public"))
-  http.Handle("/", fs)
+	// FileServer will serve all the files in the public folder as a file server
+	fs := http.FileServer(http.Dir("public"))
 
-  fmt.Println("Starting server on port 8000")
-  err := http.ListenAndServe(":8000", nil)
-  if err != nil {
-    fmt.Println("ListenAndServe: ", err)
-  }
+	b := &Broker{
+		ConnMap:   make(map[*websocket.Conn]bool),
+		Broadcast: make(chan Payload),
+		Upgrader:  &websocket.Upgrader{},
+	}
+
+  // this is a blocking call (forever loop), so we need to add a new thread!
+  go b.handleBroadcast()
+
+	http.Handle("/", fs)
+	// HandleFunc will call when the user hit the endpoint, there will be a new HandleFunc in the getHandleConnections fn.
+	// But there is only one broker. Which means only one Broadcast channel.
+	http.HandleFunc("/ws", b.getHandleConnections())
+
+	fmt.Println("Starting server on port 8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		fmt.Println("ListenAndServe: ", err)
+	}
 }
